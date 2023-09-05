ee6890e2fe69604f95f79e15ff85a3249f5286e3
shubamjagtap <smjdevopservices@gmail.com>
2023-09-05 21:22:37 +0530

    added functionality to use mongodb

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,16 +1,31 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/shubmjagtap/goSpotify/backend/database"
+	"github.com/shubmjagtap/goSpotify/backend/server"
+)
+
 func main() {
+	// Initialize the database
+	client, ctx, cancel, err := database.InitDb()
+	if err != nil {
+		fmt.Println("Failed to initialize MongoDB:", err)
+		return
+	}
+	defer database.Close(client, ctx, cancel)
+
 	// Create a router
-	router := createRouter()
+	router := server.CreateRouter()
 
 	// Create routes for the router
-	createRoutes(router)
+	server.CreateRoutes(router)
 
 	// Setup CORS middleware
-	corsRouter := setupCorsMiddleware(router)
+	corsRouter := server.SetupCorsMiddleware(router)
 
 	// Start the server
 	port := "8081"
-	startServer(port, corsRouter)
+	server.StartServer(port, corsRouter)
 }
---
+++ backend/database/database.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func Close(client *mongo.Client, ctx context.Context, cancel context.CancelFunc) {
+	defer cancel()
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+}
+
+func Connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	return client, ctx, cancel, err
+}
+
+func Ping(client *mongo.Client, ctx context.Context) error {
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("Connected successfully to MongoDB")
+	return nil
+}
+
+func InitDb() (*mongo.Client, context.Context, context.CancelFunc, error) {
+	client, ctx, cancel, err := Connect("mongodb://localhost:27017")
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	defer func() {
+		if err := Ping(client, ctx); err != nil {
+			panic(err)
+		}
+	}()
+	return client, ctx, cancel, nil
+}
